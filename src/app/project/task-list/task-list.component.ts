6fd6386bb7be0262f6d75472d7331f53e4599b8a
subhajit <subhajitbhattacharya08@gmail.com>
2025-04-22 09:42:40 +0530

    Updated task ts files to make it dynamic

--- src/app/project/task-list/task-list.component.ts
+++ src/app/project/task-list/task-list.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Task } from '../../task.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+  tasks: Task [] = [{
+    id: 1,
+    name: "Design Wireframe",
+    description: "",
+    isCompleted: false,
+    dueDate: new Date("2026-01-05"),
+    projectId: 1
 
+  },
+  { id: 2,
+    name: "Develop Frontend",
+    description: "",
+    isCompleted: true,
+    dueDate: new Date("2025-01-15"),
+    projectId: 1
+  },
+  {
+    id: 3,
+    name: "Develop Backend",
+    description: "",
+    isCompleted: false,
+    dueDate: new Date("2026-01-19"),
+    projectId: 1
+  }]
 }
